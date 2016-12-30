3a8306730ad4892ede6d9bbd85f1744956c782b9
FokHam <zhoufanghan@live.com>
2016-12-30 15:06:30 +0800

    edit ball

--- src/components/SuspendBall.vue
+++ src/components/SuspendBall.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="user-function" :class="{ani:moveAni}" @touchmove="logoMove" :style="{transform:'translate('+logoPositionX+'px,'+logoPositionY+'px)'}" @touchend="logoReposition" @touchstart="moveAni=false" @click="navDis=!navDis">
   <div class="logo">logo</div>
-  <div class="memu" :class="{active:navDis}" :style="navStyle">
+  <div class="memu" :class="{active:navDis,left:whichSide=='left',right:whichSide=='right'}">
     <ul>
       <li class="n1"><router-link to="/home">首页</router-link></li>
       <li class="n2"><router-link to="/home">客服</router-link></li>
@@ -19,13 +19,13 @@ export default{
   },
   data:function(){
     return{
+      whichSide:'left',
       moveAni:false,
       logoPositionX:0,
       logoPositionY:500,
       screenHeight:0,
       screenWidth:0,
-      navDis:false,
-      navStyle: {left: '25px'}
+      navDis:false
     }
   },
   methods:{
@@ -36,18 +36,18 @@ export default{
       var touchY = e.changedTouches[0].clientY
       if (touchX>0) {
         if (touchX < this.screenWidth) {
-          this.logoPositionX = touchX-22
+          this.logoPositionX = touchX-27
         }else {
-          this.logoPositionX = this.screenWidth-44
+          this.logoPositionX = this.screenWidth-54
         }        
       }else {
         this.logoPositionX = 0
       }
       if (touchY>0) {        
         if (touchY < this.screenHeight) {
-          this.logoPositionY = touchY-22
+          this.logoPositionY = touchY-27
         }else {
-          this.logoPositionY = this.screenHeight-44
+          this.logoPositionY = this.screenHeight-54
         } 
       }else {
         this.logoPositionY = 0
@@ -58,16 +58,16 @@ export default{
       if (this.logoPositionX <= this.screenWidth/2) {
         this.logoPositionX = 0
       }else {
-        this.logoPositionX = this.screenWidth - 44
+        this.logoPositionX = this.screenWidth - 54
       }           
     }
   },
   watch:{
     logoPositionX:function(str){
       if (str <= this.screenWidth/2) {
-        this.navStyle = {left: '25px'}
+        this.whichSide = 'left'
       }else {
-        this.navStyle = {right: '25px'}
+        this.whichSide = 'right'
       }
     }
   }
@@ -77,28 +77,35 @@ export default{
 <style lang="less" scoped>
 .user-function{
   position: fixed;top: 0;left: 0;z-index: 1000;
-  height: 44px;width: 44px;
+  height: 54px;width: 54px;
   &.ani{transition: ease-in-out 0.1s;}
   .logo{
     position: relative;z-index: 99;
-    height: 40px;width: 40px;border:2px solid #fff;
-    border-radius: 36px;overflow: hidden;
-    background-color: #58a6ea;color: #fff;line-height: 40px;
+    height: 50px;width: 50px;border:2px solid #fff;
+    border-radius: 50px;overflow: hidden;
+    background-color: #58a6ea;color: #fff;line-height: 50px;
     font-size: 0.7rem;text-align: center;
   }
   .memu{
     transition: ease-in-out 0.1s;
     position: absolute;top: 5px;
-    background-color: #58a6ea;height: 34px;overflow: hidden;
-    padding: 0;border-radius: 15px;width: 0px;    
-    &.active{width: 130px; padding: 0 22px;}
+    background-color: #58a6ea;height: 44px;overflow: hidden;
+    padding: 0;border-radius: 25px;width: 0px;
+    &.left{
+      left: 27px;
+      &.active{width: 130px; padding: 0 20px 0 35px;}
+    }
+    &.right{
+      right: 27px;
+      &.active{width: 130px; padding: 0 35px 0 20px;}
+    }
     li{
       float: left;
       width: 33.33%;
-      padding-top: 19px;
-      background-position: center 3px;
-      background-size: 17px;
-      background-repeat: no-repeat;      
+      padding-top: 27px;
+      background-position: center 4px;
+      background-size: 21px;
+      background-repeat: no-repeat;   
       &.n1{background-image: url('/static/images/home/icon-home.png');}
       &.n2{background-image: url('/static/images/home/icon-contact.png');}
       &.n3{background-image: url('/static/images/home/icon-user.png');}
