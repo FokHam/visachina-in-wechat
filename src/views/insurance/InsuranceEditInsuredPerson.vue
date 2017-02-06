d73c5359b7424b66d19d97dd9bdc6b752d1f6bff
Hoxton Lau <hoxtonlau@gmail.com>
2017-02-06 09:48:27 +0800

    insurant&policyholder component modified

---
+++ src/views/insurance/InsuranceEditInsuredPerson.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="edit-person" @click="idTypeSelecting = false">
+    <ul class="info-list wave-btm-bg">
+      <li class="info-item">
+        <span class="label">中文名字</span>
+        <input v-model="cName" class="info-input" type="text" placeholder="与证件保持一致" value="">
+      </li>
+      <li class="info-item">
+        <span class="label">英文名字</span>
+        <input v-model="eName" class="info-input" type="text" placeholder="例如：Zhangsan" value="">
+      </li>
+      <li class="info-item">
+        <span class="label">未选择证件类型</span>
+        <input v-model="idNo" type="text" class="number" value="" placeholder="输入正确的证件号">
+        <span class="show-more-btn"
+          :class="{selected: typeof(idType) === 'number'}"
+          @click.stop="idTypeSelecting = true">
+          {{ typeof(idType) === "number" ? idTypeSlots[idType] : "请选择证件类型"}}
+          <i class="icon-arrow-right"></i>
+        </span>
+      </li>
+      <li class="info-item" @click="openBdatePicker">
+        <span class="label">出生日期</span>
+        <input v-model="birthday" class="info-input" type="text" placeholder="与证件保持一致" value="">
+      </li>
+    </ul>
+    <div class="confirm-btn"
+      @click="confirm">
+      保存
+    </div>
+    <mt-datetime-picker
+      ref="bdatepicker"
+      type="date"
+      :startDate="minimunDate"
+      :endDate="maximunDate"
+      year-format="{value} 年"
+      month-format="{value} 月"
+      date-format="{value} 日"
+      @confirm="confirmBdate">
+    </mt-datetime-picker>
+    <picker v-if="idTypeSelecting"
+      :listdata="idTypeSlots"
+      @confirm="idTypeSet"
+      @close="idTypeSelecting=false">
+    </picker>
+  </div>
+</template>
+
+<script>
+  import Picker from '../../components/Picker';
+
+  export default {
+    data () {
+      const minimunDate = new Date(1900, 0, 1);
+      const maximunDate = new Date();
+      return {
+        id: "",
+        cName: "",
+        eName: "",
+        idType: "",
+        idNo: "",
+        birthday: "",
+        minimunDate: minimunDate,
+        maximunDate: maximunDate,
+        idTypeSlots: ["身份证", "军官证", "护照"],
+        idTypeSelecting: false
+      }
+    },
+    methods: {
+      openBdatePicker () {
+          this.$refs.bdatepicker.open();
+      },
+      confirmBdate (day) {
+        let m = day.getMonth() > 8 ? day.getMonth() + 1 : "0" + (day.getMonth() + 1),
+            d = day.getDate() > 9 ? day.getDate() : "0" + day.getDate();
+        this.birthday = day.getFullYear() + "-" + m + "-" + d;
+      },
+      confirm () {
+        //保存信息
+        let url = "/api/insurance/add-people";
+        let send = {
+          id: 0,
+          type: "insurant",
+          name: this.cName,
+          spell: this.eName,
+          id_type: 1,
+          id_number: this.idNo,
+          birthday: this.birthday
+        };
+        this.$http.get(url, {params: send}).then((response) => {
+          console.log(JSON.parse(response.body));
+          let body = JSON.parse(response.body);
+          if (body.status === 1) {
+            this.$emit("confirm");
+          }
+        }, (response) => {
+          console.log("服务器错误！");
+        });
+      },
+      idTypeSet (v) {
+        this.idType = this.idTypeSlots.indexOf(v);
+      }
+    },
+    components: {
+      Picker
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .icon-arrow-right {
+    display: inline-block;
+    width: 0.5rem;
+    height: 0.5rem;
+    background: url(/static/images/insurance/arrow-r.png) center no-repeat;
+    background-size: contain;
+  }
+  .edit-person {
+    position: fixed;
+    top: 0; left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #f6f6f6;
+  }
+  .info-list {
+    margin: 0 0.5rem;
+    padding-bottom: 1rem;
+    background-color: #fff;
+    .info-item {
+      position: relative;
+      display: flex;
+      line-height: 3.2rem;
+      border-top: 0.05rem solid #eee;
+      margin: 0 0.5rem;
+      &:first-child {
+        border-top: none;
+      }
+      .label {
+        flex: 1;
+      }
+      .info-input {
+        flex: 1;
+        border: none;
+        font-size: 0.7rem;
+        text-align: right;
+        margin-right: 0.5rem;
+      }
+      .show-more-btn {
+        color: #a9a9a9;
+        &.selected {
+          color: #333;
+        }
+      }
+      .number {
+        border: none;
+        line-height: 1;
+        font-size: 0.7rem;
+        position: absolute;
+        top: 2rem;
+        width: 10rem;
+        color: #999;
+      }
+    }
+  }
+  .confirm-btn {
+    margin-top: 3rem;
+  }
+  .picker {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    background-color: #fff;
+  }
+</style>
---
+++ src/views/insurance/InsuranceSelectPolicyHolder.vue
@@ -0,0 +1,201 @@
+<template>
+  <div class="sp-page">
+    <div class="sp-page-wrapper">
+      <div class="tip">最多可选择1个投保人</div>
+      <div @click="editingPerson=true" class="add-button">
+        <i class="icon-addperson-big"></i>
+        <span>添加投保人</span>
+      </div>
+      <ul class="person-list wave-btm-bg">
+        <li v-for="(person, index) in pList"
+            @click="togglePolicyHolder(person.id)"
+            class="person-item">
+          <i :class="{ on: person.id === selectedPolicyHolderId }" class="icon-radio"></i>
+          <div class="info-box">
+            <p class="name">{{ person.cName }}<span class="detail">{{ person.phone }}</span></p>
+            <p class="item">{{ person.email }}</span></p>
+          </div>
+          <i class="icon-edit"
+             @click.stop="editingPerson=true"></i>
+        </li>
+      </ul>
+      <div class="confirm-button"
+        @click="confirm">
+        确定
+      </div>
+      <edit-policy-holder
+        v-if="editingPerson"
+        v-on:confirm="editingPerson=false">
+      </edit-policy-holder>
+    </div>
+  </div>
+</template>
+
+<script>
+  import EditPolicyHolder from './InsuranceEditPolicyHolder'
+
+  export default {
+    data: function () {
+      return {
+        editingPerson: false,
+        idTypeList: ["身份证", "军官证", "护照"],
+        pList: [
+          {
+            id: 77,
+            cName: "黄旭光",
+            eName: "Huangxuguang",
+            gender: 1,
+            idType: 0,
+            idNo: "441581199104280019",
+            birthday: "1991-04-28",
+            phone: "13480101603",
+            email: "hoxtonlau@gmail.com"
+          },{
+            id: 11,
+            cName: "刘德华",
+            eName: "Liudehua",
+            gender: 1,
+            idType: 0,
+            idNo: "441581199104280019",
+            birthday: "1991-04-28",
+            phone: "13512312361",
+            email: "ldehua@126.com"
+          },{
+            id: 61,
+            cName: "张学友",
+            eName: "Zhangxueyou",
+            gender: 1,
+            idType: 0,
+            idNo: "441581199104280019",
+            birthday: "1991-04-28",
+            phone: "15822138888",
+            email: "zxueyou@hotmail.com"
+          },
+        ]
+      };
+    },
+    methods: {
+      confirm () {
+        let that = this;
+        that.pList.map(function (obj) {
+          if (obj.id === that.$store.state.insurance.selectedPolicyHolderId) {
+            that.$store.commit("confirmPolicyHolder", obj);
+          }
+        });
+        this.$router.go(-1);
+      },
+      togglePolicyHolder (id) {
+        this.$store.commit("togglePolicyHolder", id);
+      }
+    },
+    components: {
+      EditPolicyHolder
+    },
+    computed: {
+      selectedPolicyHolderId () {
+        return this.$store.state.insurance.selectedPolicyHolderId;
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .sp-page {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0; left: 0;
+    overflow-y: scroll;
+    background-color: #f6f6f6;
+  }
+  .icon-radio {
+    position: absolute;
+    top: 0.7rem;
+    left: 0rem;
+    width: 0.8rem;
+    height: 0.8rem;
+    background: url(/static/images/insurance/unselected.png) no-repeat;
+    background-size: contain;
+    &.on {
+      background-image: url(/static/images/insurance/selected.png);
+    }
+  }
+  .icon-edit {
+    position: absolute;
+    width: 0.8rem;
+    height: 0.8rem;
+    right: 0rem;
+    top: 0.7rem;
+    background: url(/static/images/insurance/edit.png) no-repeat;
+    background-size: contain;
+  }
+  .tip {
+    text-align: center;
+    line-height: 1.8rem;
+    font-size: 0.7rem;
+    color: #ffe6eb;
+    background: linear-gradient(to right, #F057AD , #BE6AEF);
+  }
+  .add-button {
+    display: block;
+    position: relative;
+    background: #fff;
+    line-height: 2.5rem;
+    span {
+      margin-left: 3.2rem;
+      font-size: 0.8rem;
+      color: #008BE4;
+    }
+    .icon-addperson-big {
+      position: absolute;
+      left: 1.2rem;
+      top: 0; bottom: 0;
+      margin: auto;
+      width: 1.5rem;
+      height: 1.3rem;
+      background: url(/static/images/insurance/add-big.png) no-repeat;
+      background-size: contain;
+    }
+  }
+  .person-list {
+    background-color: #fff;
+    margin: 0.5rem 0.5rem;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    .person-item {
+      padding: 0.7rem 0;
+      margin: 0 0.7rem;
+      position: relative;
+      border-top: 1px solid #eee;
+      &:first-child {
+        border-top: none;
+      }
+      .info-box {
+        position: relative;
+        margin-left: 1.2rem;
+        line-height: 1;
+        p {
+          margin-bottom: 0.3rem;
+        }
+        p, span {
+          color: #666;
+        }
+        .name {
+          font-size: 0.8rem;
+        }
+        .detail {
+          margin-left: 0.3rem;
+        }
+      }
+    }
+  }
+  .confirm-button {
+    margin: 1.8rem 0.5rem;
+    background-color: #008be4;
+    text-align: center;
+    color: #fff;
+    line-height: 1.8rem;
+    border-radius: 0.2rem;
+    font-size: 0.85rem;
+  }
+</style>
